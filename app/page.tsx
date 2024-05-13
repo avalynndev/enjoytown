56215cb8444c302ab04b359bffa8e9d802bc5cff
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-13 09:56:37 +0000

    improved main page as per request

--- app/page.tsx
+++ app/page.tsx
@@ -23,28 +23,8 @@ export default function Home() {
             level up your movie streaming experience.
           </p>
         </div>
-        <div className="flex gap-4">
-          <Link
-            type="link"
-            href={siteConfig.links.enjoytown}
-            target="_blank"
-            rel="noreferrer"
-            className={buttonVariants()}
-          >
-            Enjoytown
-          </Link>
-          <Link
-            type="link"
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.github}
-            className={buttonVariants({ variant: "outline" })}
-          >
-            GitHub
-          </Link>
-        </div>
       </section>
-      <main className="flex flex-col items-center justify-between p-16">
+      <main className="flex flex-col items-center justify-between pt-8">
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -56,7 +36,7 @@ export default function Home() {
           />
         </div>
 
-        <div className="lg:pt-56 md:pt-16 pt-14 grid text-center sm:pt-16 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <div className="lg:pt-16 md:pt-16 pt-16 grid text-center sm:pt-16 lg:w-full lg:max-w-7xl lg:grid-cols-4 lg:text-left lg:gap-8">
           <button
             onClick={() => router.push("/movie")}
             className="text-left group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
