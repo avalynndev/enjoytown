35e293c00491baf652034d53c6a73e002b68096a
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-15 06:42:03 +0000

    remove unused

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,6 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
