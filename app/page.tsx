8d2c2aab003345d45cdbc9509c12878dcae674a0
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-17 17:36:21 +0530

    remove text

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,6 @@ export default function Home() {
   return (
     <>
       <SiteHeader />
-      <section className="container grid items-center gap-6 md:py-10">
-        <div className="flex max-w-[900px] flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-            One stop for all your
-            <br className="hidden sm:inline" />
-            streaming and reading desires.
-          </h1>
-          <p className="max-w-[700px] text-lg text-muted-foreground">
-            Beautifully designed ui built with Radix UI and Tailwind CSS, will
-            level up your movie streaming experience.
-          </p>
-        </div>
-      </section>
       <main className="flex flex-col items-center justify-between pt-8">
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
           <Image
