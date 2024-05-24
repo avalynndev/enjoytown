3e4846da0167febc2ca42c079c95f2c5c20307f6
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-24 17:37:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
             src="/safari-pinned-tab.svg"
-            alt="Next.js Logo"
+            alt="Logo"
             width={180}
             height={37}
             priority
@@ -69,6 +69,25 @@ export default function Home() {
             </p>
             <p className="m-0 max-w-[30ch] text-sm opacity-50">
               Emotions Await!
+            </p>
+          </button>
+          
+          <button
+            onClick={() => router.push("/tv")}
+            className="text-left group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              TV shows{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              Live on TV Shows:
+            </p>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              Addiction await ≥
             </p>
           </button>
 
