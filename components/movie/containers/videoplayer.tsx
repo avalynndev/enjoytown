f7e65b310d29acd13a01bb39aecc0d577c073d90
Avalynn <avalynndev@gmail.com>
2024-06-19 18:22:24 +0530

    made better info page ui

--- components/movie/containers/videoplayer.tsx
+++ components/movie/containers/videoplayer.tsx
@@ -1,75 +1,71 @@
 "use client";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function VideoPlayer({ id }:any) {
+type VideoSourceKey =
+  | "vidsrc"
+  | "vidsrcvip"
+  | "vidsrcpro"
+  | "vidsrcin"
+  | "superembed";
+
+export default function VideoPlayer({ id }: any) {
+  const [selectedSource, setSelectedSource] =
+    useState<VideoSourceKey>("vidsrc");
+  const [loading, setLoading] = useState(false)
+
+  const videoSources: Record<VideoSourceKey, string> = {
+    vidsrc: `https://vidsrc.icu/embed/movie/${id}`,
+    vidsrcvip: `https://vidsrc.vip/embed/movie/${id}`,
+    vidsrcpro: `https://vidsrc.pro/embed/movie/${id}`,
+    vidsrcin: `https://vidsrc.in/embed/movie/${id}`,
+    superembed: `https://multiembed.mov/?video_id=${id}&tmdb=1`,
+  };
+
+  const handleSelectChange = (value: VideoSourceKey) => {
+    setLoading(true)
+    setTimeout(() => {
+      setSelectedSource(value);
+      setLoading(false);
+    }, 1000);
+  };
+
   return (
     <div className="pb-8">
-      <Tabs defaultValue="vidsrc">
-        <div className="flex flex-col items-center">
-          <TabsList>
-            <TabsTrigger value="vidsrc">VidSrc</TabsTrigger>
-            <TabsTrigger value="vidsrcvip">VidSrc.Vip</TabsTrigger>
-            <TabsTrigger value="vidsrcpro">Vidsrc.pro</TabsTrigger>
-            <TabsTrigger value="vidsrcin">Vid.In</TabsTrigger>
-            <TabsTrigger value="superembed">SuperEmbed</TabsTrigger>
-          </TabsList>
-        </div>
-        <TabsContent value="vidsrc">
-          <iframe
-            src={`https://vidsrc.icu/embed/movie/${id}`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-        </TabsContent>
-        <TabsContent value="vidsrcvip">
-          <iframe
-            src={`https://vidsrc.vip/embed/movie/${id}`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-        </TabsContent>
-        <TabsContent value="vidsrcpro">
-          <iframe
-            src={`https://vidsrc.pro/embed/movie/${id}`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-        </TabsContent>
-        <TabsContent value="vidsrcin">
-          <iframe
-            src={`https://vidsrc.in/embed/movie/${id}`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-        </TabsContent>
-        <TabsContent value="superembed">
-          <iframe
-            src={`https://multiembed.mov/?video_id=${id}&tmdb=1`}
-            referrerPolicy="origin"
-            allowFullScreen
-            width="100%"
-            height="450"
-            scrolling="no"
-            className="max-w-3xl mx-auto px-4 pt-10"
-          ></iframe>
-        </TabsContent>
-      </Tabs>
+      <div className="flex flex-col ">
+        <Select onValueChange={handleSelectChange} value={selectedSource}>
+          <SelectTrigger className="px-4 py-2 rounded-md w-[180px]">
+            <SelectValue placeholder="Select Video Source" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="vidsrc">VidSrc</SelectItem>
+            <SelectItem value="vidsrcvip">VidSrc.Vip</SelectItem>
+            <SelectItem value="vidsrcpro">Vidsrc.pro</SelectItem>
+            <SelectItem value="vidsrcin">Vid.In</SelectItem>
+            <SelectItem value="superembed">SuperEmbed</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      {loading ? (
+        <Skeleton className="mx-auto px-4 pt-10 w-full h-[500px]" />
+      ) : (
+        <iframe
+          src={videoSources[selectedSource]}
+          referrerPolicy="origin"
+          allowFullScreen
+          width="100%"
+          height="450"
+          scrolling="no"
+          className="max-w-3xl mx-auto px-4 pt-10"
+        />
+      )}
     </div>
   );
 }
---
+++ components/poster.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { Image as LucideImage } from "lucide-react";
+import { ComponentProps } from "react";
+import { cn } from "@/lib/utils";
+
+type PosterProps = {
+  url?: string;
+  alt: string;
+} & ComponentProps<"div">;
+
+export const Poster = ({ url, alt, className, ...props }: PosterProps) => {
+  return (
+    <div
+      className={cn(
+        "relative flex aspect-poster h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-muted text-muted shadow",
+        className
+      )}
+      {...props}
+    >
+      {url ? (
+        <Image
+          
+          fill
+          priority
+          sizes="100%"
+          alt={alt}
+          src={`https://image.tmdb.org/t/p/original${url}`}
+        />
+      ) : (
+        <LucideImage size={24} />
+      )}
+    </div>
+  );
+};
