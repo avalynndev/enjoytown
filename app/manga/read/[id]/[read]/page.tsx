f084c48b96264f7ff01f7650f5d8b032f63bfc38
Rounakdadsena <87689078+RounakDadsena@users.noreply.github.com>
2024-06-21 16:20:06 +0530

    Update manga read ui

--- app/manga/read/[id]/[read]/page.tsx
+++ app/manga/read/[id]/[read]/page.tsx
@@ -1,45 +1,105 @@
+'use client';
+
 import Image from "next/image";
 import { PreFetchChaterLinks } from "@/lib/fetch";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
-export default async function Read({ params }:any) {
+function Read({ params }: any) {
   const chapterId = params.read;
-  const results = await getPages(chapterId);
-  const image_base_url = results.baseUrl + "/data/" + results.chapter.hash;
-  const id = params.id;
-  const data = await getMangaInfo(id);
+  const [results, setResults] = useState<any>(null);
+  const [data, setData] = useState<any>(null);
+  const [images, setImages] = useState<string[]>([]);
+  const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  const [isChapterMenuOpen, setIsChapterMenuOpen] = useState(false);
 
-  PreFetchChaterLinks(data.chapters);
+  const fetchData = async () => {
+    const fetchedResults = await getPages(chapterId);
 
-  if (results.length === 0) {
+    if (fetchedResults && fetchedResults.chapter) {
+      const image_base_url = fetchedResults.baseUrl + "/data/" + fetchedResults.chapter.hash;
+      const id = params.id;
+      const fetchedData = await getMangaInfo(id);
+
+      PreFetchChaterLinks(fetchedData.chapters);
+
+      if (fetchedResults.length === 0) {
+        return;
+      }
+
+      const fetchedImages = fetchedResults.chapter.data.map((img: string) => {
+        return image_base_url + "/" + img;
+      });
+
+      setResults(fetchedResults);
+      setData(fetchedData);
+      setImages(fetchedImages);
+    } else {
+      console.error("Error: fetchedResults or fetchedResults.chapter is undefined");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleNextPage = () => {
+    if (currentPageIndex < images.length - 1) {
+      setCurrentPageIndex(currentPageIndex + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPageIndex > 0) {
+      setCurrentPageIndex(currentPageIndex - 1);
+    }
+  };
+
+  const toggleChapterMenu = () => {
+    setIsChapterMenuOpen(!isChapterMenuOpen);
+  };
+
+  const navigateChapter = (direction: 'prev' | 'next') => {
+    const currentIndex = data.chapters.findIndex((chapter: any) => chapter.id === chapterId);
+    let newChapter;
+    if (direction === 'prev' && currentIndex > 0) {
+      newChapter = data.chapters[currentIndex - 1];
+    } else if (direction === 'next' && currentIndex < data.chapters.length - 1) {
+      newChapter = data.chapters[currentIndex + 1];
+    }
+    if (newChapter) {
+      window.location.href = `/manga/read/${params.id}/${newChapter.id}`;
+    }
+  };
+
+  if (!results || !data || images.length === 0) {
     return (
       <div>
-        <p>This chapter has no content. Please check the next chapter.</p>
+        <p>Loading...</p>
       </div>
     );
   }
 
-  let images = [];
-  for (var i = 0; i < results.chapter.data.length; i++) {
-    var imgUrl = image_base_url + "/" + results.chapter.data[i];
-    images.push(imgUrl);
-  }
-
   return (
-    <div>
+    <div className="relative flex">
+      <Button onClick={toggleChapterMenu} className="fixed top-14 left-4 z-50">
+        {isChapterMenuOpen ? '✖' : 'VOL'}
+      </Button>
       <aside
         id="sidebar"
-        className="hidden lg:block fixed left-0 top-0 z-40 h-screen w-48 transition-transform bg-background"
+        className={`fixed left-0 top-0 z-40 h-screen w-48 transition-transform bg-background ${
+          isChapterMenuOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
         aria-label="Sidebar"
       >
         <div className="flex h-full flex-col overflow-y-auto border-slate-200 px-3 py-4">
           <div className="space-y-2 text-sm font-medium">
-            <div className="pt-12">
+            <div className="pt-20">
               <h3 className="ml-3 flex-1 text-3xl whitespace-nowrap font-mono">
                 CHAPTERS
               </h3>
-              <div className="gap-2 pt-4 flex-col items-center ">
+              <div className="gap-2 pt-4 flex-col items-center">
                 {data.chapters &&
                   data.chapters.map((item: any, index: any) => {
                     if (item.pages !== 0) {
@@ -47,7 +107,7 @@ export default async function Read({ params }:any) {
                         <div className="py-1" key={index}>
                           <Link
                             href={{
-                              pathname: `/manga/read/${id}/${item.id}`,
+                              pathname: `/manga/read/${params.id}/${item.id}`,
                             }}
                           >
                             <Button
@@ -66,28 +126,59 @@ export default async function Read({ params }:any) {
           </div>
         </div>
       </aside>
-      <div className="items-center justify-center flex flex-col">
-        {images &&
-          images.map((item, index) => (
-            <div key={index}>
-              <Image
-                src={`https://sup-proxy.zephex0-f6c.workers.dev/api-content?url=${item}&headers=https://mangadex.org`}
-                key={index}
-                alt="Pages"
-                width={800}
-                height={1000}
-                priority
-                quality={100}
-                unoptimized
-              />
-            </div>
-          ))}
+      <div className="flex-1 items-center justify-center flex flex-col">
+        <div className="flex items-center mb-4">
+          <Button
+            onClick={handlePrevPage}
+            disabled={currentPageIndex === 0}
+            className="text-white bg-black px-4 py-2 flex items-center mr-2"
+          >
+            &larr; Prev
+          </Button>
+          <span className="text-lg font-bold">{currentPageIndex + 1}</span>
+          <span className="mx-2">/</span>
+          <span className="text-lg font-bold">{images.length}</span>
+          <Button
+            onClick={handleNextPage}
+            disabled={currentPageIndex === images.length - 1}
+            className="text-white bg-black px-4 py-2 flex items-center ml-2"
+          >
+            Next &rarr;
+          </Button>
+        </div>
+        <div key={currentPageIndex}>
+          <Image
+            src={`https://sup-proxy.zephex0-f6c.workers.dev/api-content?url=${images[currentPageIndex]}&headers=https://mangadex.org`}
+            alt="Pages"
+            width={800}
+            height={1000}
+            priority
+            quality={100}
+            unoptimized
+          />
+        </div>
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-4 mb-4">
+          <Button
+            onClick={() => navigateChapter('prev')}
+            disabled={data.chapters.findIndex((chapter: any) => chapter.id === chapterId) <= 0}
+            className="text-black bg-transparent border border-black px-4 py-2 flex items-center"
+          >
+            &larr; Prev Ch
+          </Button>
+          <Button
+            onClick={() => navigateChapter('next')}
+            disabled={data.chapters.findIndex((chapter: any) => chapter.id === chapterId) >= data.chapters.length - 1}
+            className="text-black bg-transparent border border-black px-4 py-2 flex items-center"
+          >
+            Next Ch &rarr;
+          </Button>
+        </div>
       </div>
     </div>
   );
 }
 
-async function getPages(id:any) {
+async function getPages(id: any) {
   const res = await fetch(`https://api.mangadex.org/at-home/server/${id}`);
   const data = await res.json();
   return data;
@@ -101,3 +192,5 @@ async function getMangaInfo(id: any) {
   const data = await res.json();
   return data;
 }
+
+export default Read;
