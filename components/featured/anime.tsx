8e5eef25fb08a11422a1a1847067ec99bea4a465
ctrlsam <samredmondtech@gmail.com>
2024-11-02 01:33:31 +1300

    fix(anime): recent showing ?

--- components/featured/anime.tsx
+++ components/featured/anime.tsx
@@ -20,7 +20,7 @@ type AnimeFeatureProps = {
 };
 
 
-export default function FeaturedAnime({featureType} : AnimeFeatureProps) {
+export default function FeaturedAnime({ featureType }: AnimeFeatureProps) {
   const [data, setData] = React.useState<IAnimeResult[] | null>(null);
   const [loading, setLoading] = React.useState(true);
 
@@ -56,77 +56,79 @@ export default function FeaturedAnime({featureType} : AnimeFeatureProps) {
         <div className="grid w-full grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3">
           {loading
             ? // Skeleton component while loading
-              Array.from({ length: 18 }).map((_, index) => (
-                <div key={index} className="w-full space-y-2">
-                  <Skeleton className="aspect-video w-full rounded-md" />
-                  <div className="space-y-1.5">
-                    <Skeleton className="h-6 w-3/4" />
-                    <Skeleton className="h-4 w-1/2" />
-                    <Skeleton className="h-3 w-full" />
-                  </div>
+            Array.from({ length: 18 }).map((_, index) => (
+              <div key={index} className="w-full space-y-2">
+                <Skeleton className="aspect-video w-full rounded-md" />
+                <div className="space-y-1.5">
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="h-4 w-1/2" />
+                  <Skeleton className="h-3 w-full" />
                 </div>
-              ))
+              </div>
+            ))
             : data &&
-              data.map((item, index) => (
-                <Link
-                  href={`/anime/${encodeURIComponent(item.id)}`}
-                  key={index}
-                  className="w-full cursor-pointer space-y-2"
-                  data-testid="movie-card"
-                >
-                  <div className="relative flex aspect-video w-full items-center justify-center overflow-hidden rounded-md border bg-background/50 shadow">
-                    {item.image ? (
-                      <Image
-                        fill
-                        className="object-cover"
-                        src={`${PROXY}${item.image}`}
-                        alt={
-                          typeof item.title === "string"
-                            ? item.title
-                            : item.title.english ||
-                              item.title.userPreferred ||
-                              item.title.romaji ||
-                              item.title.native ||
-                              ""
-                        }
-                        sizes="100%"
-                      />
-                    ) : (
-                      <ImageIcon className="text-muted" />
-                    )}
-                  </div>
+            data.map((item, index) => (
+              <Link
+                href={`/anime/${encodeURIComponent(item.id)}`}
+                key={index}
+                className="w-full cursor-pointer space-y-2"
+                data-testid="movie-card"
+              >
+                <div className="relative flex aspect-video w-full items-center justify-center overflow-hidden rounded-md border bg-background/50 shadow">
+                  {item.image ? (
+                    <Image
+                      fill
+                      className="object-cover"
+                      src={`${PROXY}${item.image}`}
+                      alt={
+                        typeof item.title === "string"
+                          ? item.title
+                          : item.title.english ||
+                          item.title.userPreferred ||
+                          item.title.romaji ||
+                          item.title.native ||
+                          ""
+                      }
+                      sizes="100%"
+                    />
+                  ) : (
+                    <ImageIcon className="text-muted" />
+                  )}
+                </div>
 
-                  <div className="space-y-1.5">
-                    <div className="flex items-start gap-1 justify-between">
-                      <div className="justify-start">
-                        <span className="trucate line-clamp-1">
+                <div className="space-y-1.5">
+                  <div className="flex items-start gap-1 justify-between">
+                    <div className="justify-start">
+                      <span className="trucate line-clamp-1">
                         {typeof item.title === "string"
-                            ? item.title
-                            : item.title.english ||
-                              item.title.userPreferred ||
-                              item.title.romaji ||
-                              item.title.native ||
-                              ""}
-                        </span>
-                      </div>
-                      <div className="justify-end flex items-center gap-2">
+                          ? item.title
+                          : item.title.english ||
+                          item.title.userPreferred ||
+                          item.title.romaji ||
+                          item.title.native ||
+                          ""}
+                      </span>
+                    </div>
+                    <div className="justify-end flex items-center gap-2">
+                      {item.rating && (
                         <Badge variant="outline">
                           {item.rating ? item.rating / 10 : "?"}
                         </Badge>
-                        <Separator orientation="vertical" className="h-6" />
-                        <Badge variant="secondary">
-                          {item.totalEpisodes ? item.totalEpisodes : "?"}
-                        </Badge>
-                      </div>
+                      )}
+                      <Separator orientation="vertical" className="h-6" />
+                      <Badge variant="secondary">
+                        {item.episodeNumber ? item.episodeNumber : (item.totalEpisodes ?? "?")}
+                      </Badge>
                     </div>
-                   
-                    <p className="line-clamp-3 text-xs text-muted-foreground">
-                      {/* Recent only includes ep. title and others only have description */}
-                      {item.episodeTitle}{item.description}
-                    </p>
                   </div>
-                </Link>
-              ))}
+
+                  <p className="line-clamp-3 text-xs text-muted-foreground">
+                    {/* Recent only includes ep. title and others only have description */}
+                    {item.episodeTitle}{item.description}
+                  </p>
+                </div>
+              </Link>
+            ))}
         </div>
       </div>
     </main>
