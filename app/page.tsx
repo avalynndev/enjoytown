dbfc8fe04b5d0b4d19ecfb148819fee2937e077e
dev1Abun <mavenphofficial@gmail.com>
2024-12-21 19:55:47 +0800

    Sync from enjoytown.pro

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,14 @@ export default async function Home() {
       <div className="mx-auto max-w-4xl p-4">
         <section className="flex h-[75vh] items-center md:h-[50vh]">
           <div className="mx-auto flex w-4/5 flex-col items-center justify-center space-y-4 text-center">
-            {/**  
             <Link
               href={`/`}
               className="flex items-center gap-2 rounded-full border bg-background px-4 py-2 text-xs shadow"
             >
-              a{" "}
               <span className="animate-shine rounded-full border bg-[linear-gradient(110deg,#ffffff,45%,#f1f1f1,55%,#ffffff)] bg-[length:200%_100%] px-2 py-[1px] text-[10px] text-foreground dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-white">
-                PRO
+                Welcome to the town of enjoyment!
               </span>
             </Link>
-          **/}
             <h1 className="text-6xl font-bold">Explore movies, tv series, and animes!</h1>
             <p className="text-sm leading-6 text-muted-foreground">
               EnjoyTown is a streaming platform for lazy people who like to
@@ -28,8 +25,8 @@ export default async function Home() {
               watch millions of movies, series and animes for free. Go down to watch
             </p>
             <div className="flex gap-2">
-              <Button disabled>
-                <Link href={`/auth/register`}>Sign up</Link>
+              <Button>
+                <Link href={`/movie`}>Watch Now</Link>
               </Button>
               <Link href={`/changelog`}>
                 <Button variant="outline">Changelog</Button>
